1bbd059e3324fad6948b6459aa60dc0603129b44
Roy.Xu <bbb233456@gmail.com>
2023-10-08 06:19:04 +0800

    Update dataset getitem function

--- sinodataset.py
+++ sinodataset.py
@@ -10,30 +10,47 @@ def cast_to_float(value) -> float:
 
 class SinoDataset(torch.utils.data.dataset.Dataset):
     def __init__(self, csv_path: str):
+        self.__feature_cols = [
+            "縣市", "鄉鎮市區", "路名", "土地面積", "使用分區", "移轉層次", "總樓層數",
+            "主要用途", "主要建材", "建物型態", "屋齡", "建物面積", "車位面積",
+            "車位個數", "橫坐標", "縱坐標", "備註", "主建物面積", "陽台面積", "附屬建物面積"
+        ]
+        self.__label_cols = [
+            "單價"
+        ]
         self.__df = pd.read_csv(csv_path)
 
     def __getitem__(self, index):
-        origin_row = self.__df.iloc[index].to_list()[1:]
-        float_row = list(map(
+        feature_row = self.__df[self.__feature_cols].iloc[index].to_list()
+        lable_row = self.__df[self.__label_cols].iloc[index].to_list()
+        float_feature_row = list(map(
             cast_to_float,
-            origin_row
+            feature_row
         ))
-        return torch.Tensor(float_row[:-1]), torch.Tensor([origin_row[-1]])
+        return torch.Tensor(float_feature_row), torch.Tensor(lable_row)
     
     def __len__(self):
         return self.__df.__len__()
 
 class SinoTestDataset(torch.utils.data.dataset.Dataset):
     def __init__(self, csv_path: str):
+        self.__feature_cols = [
+            "縣市", "鄉鎮市區", "路名", "土地面積", "使用分區", "移轉層次", "總樓層數",
+            "主要用途", "主要建材", "建物型態", "屋齡", "建物面積", "車位面積",
+            "車位個數", "橫坐標", "縱坐標", "備註", "主建物面積", "陽台面積", "附屬建物面積"
+        ]
+        self.__label_cols = [
+            "單價"
+        ]
         self.__df = pd.read_csv(csv_path)
 
     def __getitem__(self, index):
-        origin_row = self.__df.iloc[index].to_list()[1:]
-        float_row = list(map(
+        feature_row = self.__df[self.__feature_cols].iloc[index].to_list()
+        float_feature_row = list(map(
             cast_to_float,
-            origin_row
+            feature_row
         ))
-        return torch.Tensor(float_row), 0
+        return torch.Tensor(float_feature_row), 0
     
     def __len__(self):
         return self.__df.__len__()

a7c7b584ebde6c44a431a2c946de78fcab72a6d3
Roy.Xu <bbb233456@gmail.com>
2023-10-07 16:42:29 +0800

    Add data normalize to preprocessing.py

--- preprocessing.py
+++ preprocessing.py
@@ -21,21 +21,43 @@ def get_encoded_list(origin_list: list) -> dict:
     )(origin_list)
 
 if __name__ == "__main__":
-    # Init
-    train_data = pd.read_csv ('data/training_data.csv')
-    update_cols = { "縣市": {}, "鄉鎮市區": {}, "路名": {}, "使用分區": {}, "主要用途": {}, "主要建材": {} }
+    #
+    # 初始化資料以及將備註的空欄位填 0
+    #
+    train_data = pd.read_csv ('data/public_dataset.csv')
+    train_data["備註"] = train_data["備註"].fillna("0")
 
-    # Get encode dictionary
+    #
+    # 取得編碼表
+    #
+    update_cols = { "縣市": {}, "鄉鎮市區": {}, "路名": {}, "使用分區": {}, "主要用途": {}, "主要建材": {}, "備註": {} }
     for key in update_cols.keys():
         encoded_number_list = get_encoded_list(train_data[key])
         update_cols.get(key).update(encoded_number_list)
 
-    # Update every row col value
+    #
+    # 將編碼表對應到欄位上更新
+    #
     for index, row in train_data.iterrows():
         for key, value in update_cols.items():
             row_col_value = row.get(key)
             encoded_number = value.get(row_col_value)
             train_data.at[index, key] = encoded_number
 
-    # Save
-    train_data.to_csv("data/training_revised.csv", index=False)
+
+    #
+    # 對該欄位做歸一化
+    #
+    normalized_cols = [
+        "縣市", "鄉鎮市區", "路名", "土地面積", "使用分區", "移轉層次",
+        "總樓層數", "主要用途", "主要建材", "屋齡", "建物面積", "車位面積", "車位個數",
+        "橫坐標", "縱坐標", "備註", "主建物面積", "陽台面積", "附屬建物面積"
+    ]
+    for col in normalized_cols:
+        train_data[col] = (train_data[col] - train_data[col].mean()) / train_data[col].std()
+
+    
+    #
+    # 保存更動
+    #
+    train_data.to_csv("data/public_revised.csv", index=False)

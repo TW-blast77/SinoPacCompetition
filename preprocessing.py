fc36895ab47b41e838155280a41a44193d10c1a2
Roy.Xu <bbb233456@gmail.com>
2023-10-09 20:26:57 +0800

    Add external data to training, Update models

--- preprocessing.py
+++ preprocessing.py
@@ -1,63 +1,133 @@
 import pandas as pd
 import ramda as R
+import numpy as np
 
-def get_encoded_list(origin_list: list) -> dict:
+def get_encoded_dict(df, encoded_col_list) -> dict:
     """
-        Args:
-            origin_list:
-                a list with different item ex: ["A", "B", "A", "C", "A", "B"]
-        Returns:
-            a encoded dict ex: { "A": 0, "B": 1, "C": 2 }
+    Args:
+        df:
+            Dataframe object read from training csv
+        encoded_col_list:
+            List with different item
+            ex: ["縣市", "鄉鎮市區", "路名", "使用分區"]
+    Returns:
+        Encoded dict
+        ex:
+            {
+                "縣市": {"台北市": 0, "台中市": 1 ...},
+                "鄉鎮市區": {"大安區": 0, "萬華區": 1 ...}
+                ...
+            }
     """
-    return R.pipe(
-        sorted,
-        R.group_with(R.equals),
-        R.map(set),
-        R.map(lambda city: str(city.pop())),
-        enumerate,
-        list,
-        R.map(lambda x: (x[1], x[0])),
-        R.from_pairs
-    )(origin_list)
+    res = {}
+    for key in encoded_col_list:
+        res[key] = R.pipe(
+            R.map(str),
+            sorted,
+            R.group_with(R.equals),
+            R.map(set),
+            R.map(lambda city: str(city.pop())),
+            enumerate,
+            list,
+            R.map(lambda x: (x[1], x[0])),
+            R.from_pairs
+        )(df[key])
+    return res
+
+def cal_external_data_nearest_distance(org_df, ex_df) -> list:
+    """
+    Args:
+        org_df:
+            Dataframe object read from training csv
+        ex_df:
+            Dataframe object read from external data
+    Returns:
+        List with each line have the nearest distance
+        ex: [ 65.2, 63, 12, 84, 100, 632, 14, ... ]
+    """
+    nearest_distance_list = []
+    for index in range(org_df.__len__()):
+        building_x = org_df["橫坐標"].iloc[index]
+        building_y = org_df["縱坐標"].iloc[index]
+        nearest_distance = np.min(
+            np.sqrt(
+                (ex_df["橫坐標"] - building_x) ** 2 + (ex_df["縱坐標"] - building_y) ** 2
+            )
+        )
+        nearest_distance_list.append(nearest_distance)
+    return nearest_distance_list
+
+def cal_external_data_count(org_df, ex_df, distance=1000) -> list:
+    """
+    Args:
+        org_df:
+            Dataframe object read from training csv
+        ex_df:
+            Dataframe object read from external data
+    Returns:
+        List with each line have the quantity of the facility
+        ex: [ 10, 5, 3, 7, 5, 3 ]
+    """
+    near_count_list = []
+    for index in range(org_df.__len__()):
+        building_x = org_df["橫坐標"].iloc[index]
+        building_y = org_df["縱坐標"].iloc[index]
+        distance_array = np.sqrt((ex_df["橫坐標"] - building_x) ** 2 + (ex_df["縱坐標"] - building_y) ** 2)
+        nearest_distance = np.count_nonzero(
+            distance_array[distance_array > distance]
+        )
+        near_count_list.append(nearest_distance)
+    return near_count_list
 
 if __name__ == "__main__":
-    #
-    # 初始化資料以及將備註的空欄位填 0
-    #
-    train_data = pd.read_csv ('data/public_dataset.csv')
+    train_data = pd.read_csv('data/training_data.csv')
     train_data["備註"] = train_data["備註"].fillna("0")
 
     #
-    # 取得編碼表
+    # 編碼給定的欄位
     #
-    update_cols = { "縣市": {}, "鄉鎮市區": {}, "路名": {}, "使用分區": {}, "主要用途": {}, "主要建材": {}, "備註": {} }
-    for key in update_cols.keys():
-        encoded_number_list = get_encoded_list(train_data[key])
-        update_cols.get(key).update(encoded_number_list)
-
-    #
-    # 將編碼表對應到欄位上更新
-    #
+    update_col_list = [
+        "縣市", "鄉鎮市區", "路名", "使用分區", "主要用途",
+        "路名", "建物型態", "主要建材", "備註"
+    ]
+    encoded_col_dict = get_encoded_dict(train_data, update_col_list)
     for index, row in train_data.iterrows():
-        for key, value in update_cols.items():
+        for key, value in encoded_col_dict.items():
             row_col_value = row.get(key)
             encoded_number = value.get(row_col_value)
             train_data.at[index, key] = encoded_number
 
-
     #
-    # 對該欄位做歸一化
+    # 利用額外資料擴充特徵欄位
     #
-    normalized_cols = [
-        "縣市", "鄉鎮市區", "路名", "土地面積", "使用分區", "移轉層次",
-        "總樓層數", "主要用途", "主要建材", "屋齡", "建物面積", "車位面積", "車位個數",
-        "橫坐標", "縱坐標", "備註", "主建物面積", "陽台面積", "附屬建物面積"
+    external_data_list = [
+        "ATM資料", "便利商店", "公車站點資料", "國中基本資料", "國小基本資料",
+        "大學基本資料", "捷運站點資料", "火車站點資料", "腳踏車站點資料",
+        "郵局據點資料", "醫療機構基本資料", "金融機構基本資料", "高中基本資料"
     ]
-    for col in normalized_cols:
-        train_data[col] = (train_data[col] - train_data[col].mean()) / train_data[col].std()
+    nearest_distance_list = []
+    near_facility_count_list = []
+    for ex_data_path in external_data_list:
+        ex_pd = pd.read_csv(f"external_data/{ex_data_path}_revised.csv")
+        train_data[f"最近的{ex_data_path}"] = cal_external_data_nearest_distance(train_data, ex_pd)
+        train_data[f"方圓1km{ex_data_path}"] = cal_external_data_count(train_data, ex_pd, 1000)
+        nearest_distance_list.append(f"最近的{ex_data_path}")
+        near_facility_count_list.append(f"方圓1km{ex_data_path}")
+        print(f"資料{ex_data_path}特徵計算完成...")
 
-    
     #
-    # 保存更動
+    # 對給定的欄位做正規化
     #
-    train_data.to_csv("data/public_revised.csv", index=False)
+    normalized_cols_list = [
+        "縣市", "鄉鎮市區", "路名", "土地面積", "使用分區", "移轉層次",
+        "總樓層數", "主要用途", "主要建材", "建物型態", "屋齡", "建物面積", "車位面積", "車位個數",
+        "橫坐標", "縱坐標", "備註", "主建物面積", "陽台面積", "附屬建物面積"
+    ] + nearest_distance_list + near_facility_count_list
+    for col in normalized_cols_list:
+        train_data[col] = (train_data[col] - train_data[col].mean()) / train_data[col].std()
+        print(f"欄位{col}標準化完成...")
+
+    #
+    # 保存
+    #
+    train_data.to_csv("data/training_revised.csv", index=False)
--- train.py
+++ train.py
@@ -1,6 +1,7 @@
 from __future__ import print_function
 import os
 import torch
+import math
 import torch.nn as nn
 import torch.optim as optim
 from torch.utils.data.dataloader import DataLoader
@@ -14,8 +15,8 @@ def train(model, train_loader, loss_function, optimizer) -> float:
     iter_train_count, epoch_train_loss = 0, 0
     for data, lable in train_loader:
         model.train()
-        data = data.to(device)
-        lable = lable.to(device)
+        data = data.to(device).unsqueeze(1)
+        lable = lable.to(device).unsqueeze(1)
         yhat = model(data)
         loss = loss_function(lable, yhat)
         loss.backward()
@@ -24,15 +25,15 @@ def train(model, train_loader, loss_function, optimizer) -> float:
 
         iter_train_count+=1
         epoch_train_loss+=loss.item()
-        print(f"Iter={iter_train_count}, Iter_train_loss={loss.item()}")
     return epoch_train_loss/iter_train_count
+
 
 def valid(model, valid_loader, loss_function) -> float:
     iter_valid_count, epoch_valid_loss = 0, 0
     for data, lable in valid_loader:
         model.eval()
-        data = data.to(device)
-        lable = lable.to(device)
+        data = data.to(device).unsqueeze(1)
+        lable = lable.to(device).unsqueeze(1)
         yhat = model(data)
         loss = loss_function(lable, yhat)
 
@@ -40,24 +41,27 @@ def valid(model, valid_loader, loss_function) -> float:
         epoch_valid_loss+=loss.item()
     return epoch_valid_loss/iter_valid_count
 
+
 if __name__ == '__main__':
     load_dotenv()
-    TORCH_SEED          = int(os.getenv("TORCH_SEED", 1))
-    EPOCHS              = int(os.getenv("EPOCHS", 10))
-    LEARNING_RATE       = float(os.getenv("LEARNING_RATE", 1e-5))
-    TRAIN_DATASET_RATIO = float(os.getenv("TRAIN_DATASET_RATIO", 0.9))
-    VALID_DATASET_RATIO = float(os.getenv("VALID_DATASET_RATIO", 0.1))
-    TRAIN_BATCH_SIZE    = int(os.getenv("TRAIN_BATCH_SIZE", 64))
-    VALID_BATCH_SIZE    = int(os.getenv("VALID_BATCH_SIZE", 64))
-    TRAIN_CSV_PATH      = os.getenv("TRAIN_CSV_PATH", "")
-    TRAIN_MODEL_PT_PATH = os.getenv("TRAIN_MODEL_PT_PATH", "")
+    TORCH_SEED              = int(os.getenv("TORCH_SEED", 1))
+    EPOCHS                  = int(os.getenv("EPOCHS", 10))
+    LEARNING_RATE           = float(os.getenv("LEARNING_RATE", 1e-5))
+    TRAIN_DATASET_RATIO     = float(os.getenv("TRAIN_DATASET_RATIO", 0.9))
+    VALID_DATASET_RATIO     = float(os.getenv("VALID_DATASET_RATIO", 0.1))
+    TRAIN_BATCH_SIZE        = int(os.getenv("TRAIN_BATCH_SIZE", 64))
+    VALID_BATCH_SIZE        = int(os.getenv("VALID_BATCH_SIZE", 64))
+    TRAIN_CSV_PATH          = os.getenv("TRAIN_CSV_PATH", "")
+    TRAIN_MODEL_PT_PATH     = os.getenv("TRAIN_MODEL_PT_PATH", "")
+    TRAIN_MODEL_NAME        = os.getenv("TRAIN_MODEL_NAME", "")
+    TRAIN_BEST_MODEL_NAME   = os.getenv("TRAIN_BEST_MODEL_NAME", "")
 
 
     torch.manual_seed(TORCH_SEED)
     device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
     model = Net().to(device)
-    loss_function = nn.SmoothL1Loss()
-    optimizer = optim.Adam(model.parameters(), lr=LEARNING_RATE, betas=(0.9, 0.999), eps=1e-08, weight_decay=0)
+    loss_function = nn.MSELoss()
+    optimizer = optim.Adam(model.parameters(), lr=LEARNING_RATE, betas=(0.9, 0.999), eps=1e-8, weight_decay=1e-7)
 
 
     train_data = SinoDataset(TRAIN_CSV_PATH)
@@ -65,10 +69,14 @@ if __name__ == '__main__':
     train_loader = DataLoader(dataset=train_set, batch_size=TRAIN_BATCH_SIZE, shuffle=False, num_workers=4)
     valid_loader = DataLoader(dataset=valid_set, batch_size=VALID_BATCH_SIZE, shuffle=False, num_workers=4)
 
-
+    lowest_valid_loss = math.inf
     for epoch in range(EPOCHS):
         train_loss = train(model, train_loader, loss_function, optimizer)
         valid_loss = valid(model, valid_loader, loss_function)
-        print(f"\nEpochs={epoch}, avg_train_loss={train_loss}, avg_valid_loss={valid_loss}\n")
+        if lowest_valid_loss > valid_loss:
+            torch.save(model, TRAIN_MODEL_PT_PATH+TRAIN_BEST_MODEL_NAME)
+            lowest_valid_loss = valid_loss
+        print(f"\nEpochs={epoch}, avg_train_loss={train_loss}, avg_valid_loss={valid_loss}")
 
-    torch.save(model, TRAIN_MODEL_PT_PATH)
+
+    torch.save(model, TRAIN_MODEL_PT_PATH+TRAIN_MODEL_NAME)
